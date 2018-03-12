8a75a22b6f8031f8ff333c891847ef7c1e7b3047
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-12 16:47:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,15 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
- 
+ first = array[1]
+ second = array[2]
+ swap_elements.each_with_index.map do |swap, index|
+   if index == 1 
+     swap = second
+     elsif index == 2 
+     swap = first
+   end
+ end
  end
   
  
